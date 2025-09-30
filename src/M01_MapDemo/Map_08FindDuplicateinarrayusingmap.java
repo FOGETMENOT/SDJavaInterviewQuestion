c42dbc0a3b9d883c1b0eaf0abab4d489e6a25770
tanvi <sammiksha102@gmail.com>
2025-09-30 19:27:53 +0530

    duplicate

---
+++ src/M01_MapDemo/Map_08FindDuplicateinarrayusingmap.java
@@ -0,0 +1,20 @@
+package M01_MapDemo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_08FindDuplicateinarrayusingmap {
+public static void main(String[] args) {
+	int[] num= {1,2,3,4,5,6};
+	int count=0;
+	Map<Integer,Integer> map=new HashMap<>();
+	for(Integer num
+)
+	{
+	
+	if(count>0) {
+		map.put(word,count);
+	}
+	}
+}
+}
